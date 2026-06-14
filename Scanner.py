202525dc4b69f36398a894c4bbc319f5b14ce0ba
NADAVAPALLI-SAI-PRANAV-BHASKAR <148541471+NADAVAPALLI-SAI-PRANAV-BHASKAR@users.noreply.github.com>
2026-06-15 03:52:27 +0530

    Update Scanner.py

--- Scanner.py
+++ Scanner.py
@@ -56,7 +56,7 @@ results = []
 
 for port in ports:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(1)
+    sock.settimeout(0.1)
 
     result = sock.connect_ex((ip_address, port))
 
